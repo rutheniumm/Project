41e243839c7aef97860bcba294084c4df2391439
rutheniumm <93807418+rutheniumm@users.noreply.github.com>
2023-02-04 21:11:40 +0000

    we

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use graphul::{Graphul, http::Methods};
+
+
+#[tokio::main]
+async fn main() {
+    let mut app = Graphul::new();
+
+    app.get("/", || async {
+        "Hello, World 👋!"
+    });
+
+    app.run("127.0.0.1:8000").await;
 }
