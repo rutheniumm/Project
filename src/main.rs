31566bd65af27e2eeb75d88a8231bf9783871867
rutheniumm <93807418+rutheniumm@users.noreply.github.com>
2023-02-04 20:46:09 +0000

    Yes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main(){
-    println!("hello World");
+#[macro_use] extern crate rocket;
+
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
