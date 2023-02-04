d84c85c5db8069fdc426a6b709150f3c1dfa7b3e
rutheniumm <93807418+rutheniumm@users.noreply.github.com>
2023-02-04 21:19:00 +0000

    we

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use graphul::{Graphul, http::Methods};
 
 #[tokio::main]
-fn main() {
+async fn main() {
     let mut app = Graphul::new();
 
     app.get("/", || async {
         "Hello, World 👋!"
     });
 
-    app.run("127.0.0.1:8000");
+    app.run("127.0.0.1:8000").await;
 }
