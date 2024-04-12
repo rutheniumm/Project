585410233bfe101f579e88b7d6d1509543cb614f
rutheniumm <93807418+rutheniumm@users.noreply.github.com>
2024-04-12 19:00:10 -0400

    Update MidiPlayer.lua

--- MidiPlayer.lua
+++ MidiPlayer.lua
@@ -15,6 +15,7 @@ end
 function getNoteIndex(channel: number, note: number)
 	return channel * 128 + note + 1
 end
+local scale = {"C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2", "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3", "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4", "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5", "C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6", "A6", "A#6", "B6", "C7", "C#7", "D7", "D#7", "E7", "F7", "F#7", "G7", "G#7", "A7", "A#7", "B7"}
 
 function createNoteSound(note: number, channel: number, patch: number)
 	local sound = Instance.new("Sound")
@@ -140,6 +141,7 @@ function _player:playNote(channel: number, note: number, velocity: number)
 			sound.Looped = false
 		end
 	else
+		print(note, scale[note])
 		local info = MidiConstants.Percussion[note] or MidiConstants.Percussion[31]
 		local data = info[2]
 		local set = data.Settings or {}
