b56782e47031520cc18872573a369b618bdd0862
rutheniumm <93807418+rutheniumm@users.noreply.github.com>
2024-09-30 18:58:48 -0400

    Update MidiConstants.lua

--- MidiConstants.lua
+++ MidiConstants.lua
@@ -1,6 +1,6 @@
 return {
 	Patches = {
-		[0] = { "Piano", "Acoustic Grand", "rbxassetid://rbxassetid://16127995750", { Transpose = -8 } },
+		[0] = { "Piano", "Acoustic Grand", "rbxassetid://16127995750", { Transpose = -8 } },
 	},
 	Percussion = {	},
 } 
