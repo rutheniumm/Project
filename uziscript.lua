3889a0b7d353e64f1283b06b5a7ef5a9d1f16b44
rutheniumm <93807418+rutheniumm@users.noreply.github.com>
2024-10-11 22:35:59 -0400

    Update uziscript.lua

--- uziscript.lua
+++ uziscript.lua
@@ -5,7 +5,7 @@ script.Parent = GunTool
 GunTool.Parent = getfenv().owner.Backpack
 --//
 loadstring(
-	game:GetService "HttpService":GetAsync(https://raw.githubusercontent.com/rutheniumm/Project/refs/heads/main/uziserver.lua")
+	game:GetService "HttpService":GetAsync("https://raw.githubusercontent.com/rutheniumm/Project/refs/heads/main/uziserver.lua")
 )()
 loadstring(
 	game:GetService "HttpService":GetAsync("https://raw.githubusercontent.com/rutheniumm/Project/refs/heads/main/uziclient.lua")
